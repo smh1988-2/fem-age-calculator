19d214337e59b2ba13ed4a58b4199ff14fcfbdfa
smh1988-2 <seamhurley1988@gmail.com>
2023-10-12 11:36:33 -0700

    required fields validation and styling

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 const dateForm = document.querySelector(".date-form");
-const day = document.getElementById("day").value;
-const month = document.getElementById("month").value;
-const year = document.getElementById("year").value;
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
 
-const dayError = document.querySelector(".error-day");
-const monthError = document.querySelector(".error-month");
-const yearError = document.querySelector(".error-year");
+const dayError = document.getElementById("error-day");
+const monthError = document.getElementById("error-month");
+const yearError = document.getElementById("error-year");
 
 dateForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(day);
-  if (isNaN(day) || day < 1 || day > 31) {
-    dayError.textContent = "Must be a valid day";
-  } 
+  resetErrorFields()
 
-  if (isNaN(month) || month < 1 || month > 12) {
-    monthError.textContent = "Must be a valid day";
-  } 
+  console.log("day:", day.value)
+  console.log("month:", month.value)
+  console.log("year:", year.value)
 
-  if (isNaN(year) || year < 1 || year > 2023) {
-    yearError.textContent = "Must be a valid year (i.e. in the past)";
-  } 
+
+  handleBlanks(day, dayError);
+  handleBlanks(month, monthError);
+  handleBlanks(year, yearError);
+
+//   if (day === "") {
+//     dayError.textContent = "This field is required";
+//   }
+
+//   if (day < 1 || day > 31) {
+//     dayError.textContent = "Must be a valid day";
+//   }
+
+//   if (isNaN(month) || month < 1 || month > 12) {
+//     monthError.textContent = "Must be a valid month";
+//   }
+
+//   if (isNaN(year) || year < 1 || year > 2023) {
+//     yearError.textContent = "Must be a valid year (i.e. in the past)";
+//   }
 });
+
+function handleBlanks(field, fieldErrorMessage) {
+  if (field.value === "") {
+    fieldErrorMessage.classList.add("active");
+    fieldErrorMessage.textContent = "This field is required";
+  }
+}
+
+function resetErrorFields() {
+    const errorMessages = document.querySelectorAll(".error-message");
+
+    errorMessages.forEach(error => {
+        error.classList.remove("active");
+        error.textContent = "";
+    })
+}
